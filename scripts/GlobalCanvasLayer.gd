93c9ef1dda52ba9ca9910369de82b94a976115a3
filinsrg <1>
2020-10-14 03:00:53 +0300

    Ships and Orders UI

--- scripts/GlobalCanvasLayer.gd
+++ scripts/GlobalCanvasLayer.gd
@@ -1,12 +1,12 @@
 extends CanvasLayer
-class_name GlobalCanvasLayer
+#class_name GlobalCanvasLayer
 
 func _ready():
-	var context : ViewportWithContext = get_viewport()
-	context.register(null, "GlobalCanvasLayer", self)
-
-func set_ui(ui):
-	for c in get_children():
-		remove_child(c)
-		c.free()
-	add_child(ui)
+#	var context : ViewportWithContext = get_viewport()
+#	context.register(null, "GlobalCanvasLayer", self)
+	pass
+#func set_ui(ui):
+#	for c in get_children():
+#		remove_child(c)
+#		c.free()
+#	add_child(ui)
